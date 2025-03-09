c2cacc3430a6be958fc5498365611ae210c4c893
amirrezajm <amirreza.jolani1998@gmail.com>
2025-03-09 16:31:47 +0100

    fix the problem

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -99,15 +99,16 @@ export default function Home() {
     tawkMessengerRef.current = true;
     if (typeof window !== "undefined" && window.Tawk_API) {
       window.Tawk_API.onBeforeLoad = function () {
-        console.log("hey")
-        // Ensure chat doesn't auto-pop up
         window.Tawk_API.hideWidget();
       };
-      window.Tawk_API.onLoad = function () {
-        window.Tawk_API.showWidget();
-      }
     }
   }, []);
+
+  const openChat = () => {
+    if (window.Tawk_API) {
+      window.Tawk_API.showWidget();
+    }
+  };
 
   const handleButtonClick = (action: () => void) => {
     if (action === handleFind) {
@@ -148,13 +149,13 @@ export default function Home() {
         company: formData.company,
         name: formData.name,
       };
-    const result = await fetch("/api/mail", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userEmailData),
-    }).then((res) => res.json());
+      const result = await fetch("/api/mail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userEmailData),
+      }).then((res) => res.json());
       if (result.success) {
         console.log("Emails sent successfully!");
         // Reset form or handle success
@@ -177,6 +178,28 @@ export default function Home() {
 
   return (
     <div>
+      <div className="w-full h-full relative ">
+        <button className="bottom-5 right-5 fixed flex justify-center items-center rounded-full z-50 border-none w-[60px] h-[60px] leading-[3.75rem] bg-primary focus-visible:outline-none cursor-pointer" onClick={openChat}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 800 800"
+            height="32px"
+            width="32px"
+            role="img"
+            aria-labelledby="openIconTitle openIconDesc"
+            className="tawk-min-chat-icon"
+            fill="#fff"
+          >
+            <title id="openIconTitle">Opens Chat</title>
+            <desc id="openIconDesc">This icon Opens the chat window.</desc>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M400 26.2c-193.3 0-350 156.7-350 350 0 136.2 77.9 254.3 191.5 312.1 15.4 8.1 31.4 15.1 48.1 20.8l-16.5 63.5c-2 7.8 5.4 14.7 13 12.1l229.8-77.6c14.6-5.3 28.8-11.6 42.4-18.7C672 630.6 750 512.5 750 376.2c0-193.3-156.7-350-350-350zm211.1 510.7c-10.8 26.5-41.9 77.2-121.5 77.2-79.9 0-110.9-51-121.6-77.4-2.8-6.8 5-13.4 13.8-11.8 76.2 13.7 147.7 13 215.3.3 8.9-1.8 16.8 4.8 14 11.7z"
+            ></path>
+          </svg>
+        </button>
+      </div>
       {/* TODO: MOVE TO HEADER COMPONENT */}
       <header className="relative min-h-screen overflow-hidden bg-gradient-to-br from-purple-100 via-indigo-200 to-pink-100 dark:from-purple-900 dark:via-purple-950 dark:to-purple-900">
         {/* Transparent Header */}
