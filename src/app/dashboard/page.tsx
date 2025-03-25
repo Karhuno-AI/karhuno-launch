a97aff57132ed0b44e9edf76f733d066a95bc979
amirrezajm <amirreza.jolani@yahoo.com>
2025-03-25 15:51:43 +0100

    change the page ui and fix background and icon alignment

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { DashboardCards } from "@/components/dashboard/DashboardCards";
+
 export default function Home() {
   return (
-    <div>
-      Load videos in the future
+    <div className="flex-1 pl-6 pr-12 pt-24 container max-w-none">
+      <div className="space-y-6 p-6">
+        <h1 className="text-2xl font-bold tracking-tight">Dashboard</h1>
+        <DashboardCards />
+      </div>
+      
     </div>
   );
 }
--- src/components/home/signal-carousel.tsx
+++ src/components/home/signal-carousel.tsx
@@ -10,47 +10,33 @@ export default function SignalCarousel() {
 
   const signals = [
     {
-      icon: <Linkedin className="size-7 text-[#0A66C2]" />,
+      icon: <Linkedin className="size-6 text-[#0A66C2]" />,
       text: "A new company launched Ad Campaign",
     },
     {
-      icon: <Linkedin className="size-7 text-[#0A66C2]" />,
+      icon: <Linkedin className="size-6 text-[#0A66C2]" />,
       text: "A post on a certain topic among your network or outside of it",
     },
     {
       icon: (
-        <Image
-          src="/kickstarter.svg"
-          alt="hiring"
-          width={40}
-          height={40}
-          className="size-7"
-        />
+        <Image src="/kickstarter.svg" alt="hiring" width={46} height={46} />
       ),
-      text: "The product pledged >100%.",
+      text: "pledged 100% on Kickstarter",
     },
     {
-      icon: <DollarSign className="size-7 text-[#0A66C2]" />,
+      icon: <DollarSign className="size-6 text-[#0A66C2]" />,
       text: "A company attracted investment",
     },
     {
-      icon: <EarthIcon className="size-7 text-[#0A66C2]" />,
+      icon: <EarthIcon className="size-6 text-[#0A66C2]" />,
       text: "A company announced the purchase of green credits",
     },
     {
-      icon: <EarthIcon className="size-7 text-[#0A66C2]" />,
+      icon: <EarthIcon className="size-6 text-[#0A66C2]" />,
       text: "A company announced plans to open a warehouse",
     },
     {
-      icon: (
-        <Image
-          src="/hiring.svg"
-          alt="hiring"
-          width={40}
-          height={40}
-          className="size-8"
-        />
-      ),
+      icon: <Image src="/hiring.svg" alt="hiring" width={52} height={52} />,
       text: "A company is hiring a biochemist",
     },
     {
@@ -89,16 +75,17 @@ export default function SignalCarousel() {
   }, [isHovered]);
 
   return (
-    <div className="h-screen w-full overflow-hidden relative bg-gradient-to-tr from-purple-50 via-indigo-100 to-pink-50">
+    <section className="py-24 relative before:-z-10 before:absolute before:inset-0 before:bg-gradient-to-b before:from-purple-200/80 before:via-pink-200/60 before:to-purple-200/80 before:animate-gradient-shift before:opacity-75 overflow-hidden">
       <div className="container mx-auto flex h-full flex-col overflow-hidden">
         {/* Heading */}
         <div className="w-full pt-16 pb-8 px-4">
-          <div className="text-center mb-0">
-          <h2 className="text-3xl md:text-4xl mb-4 text-gray-900">
-            Some examples of{" "}
-            <span className="text-primary">signals we can track</span>
-          </h2>
-
+          <div className="text-center mb-8">
+            <h2 className="text-3xl md:text-4xl mb-4 text-gray-900">
+              Some examples of{" "}
+              <span className="bg-gradient-to-r from-[#792abf] to-[#522faa] text-transparent bg-clip-text">
+                signals we can track
+              </span>
+            </h2>
           </div>
         </div>
 
@@ -116,8 +103,10 @@ export default function SignalCarousel() {
                 className="flex-shrink-0 w-80 bg-white rounded-xl shadow-lg p-6 transform-gpu"
               >
                 <div className="flex flex-col items-center text-center gap-6">
-                  <div className="p-4 bg-[#f5f5ff] rounded-full flex items-center justify-center">
-                    {signal.icon}
+                  <div className="p-4 bg-[#f5f5ff] rounded-full flex items-center justify-center w-14 h-14">
+                    <div className="w-7 h-7 flex items-center justify-center">
+                      {signal.icon}
+                    </div>
                   </div>
                   <p className="text-base font-medium text-gray-700">
                     {signal.text}
@@ -128,6 +117,6 @@ export default function SignalCarousel() {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
