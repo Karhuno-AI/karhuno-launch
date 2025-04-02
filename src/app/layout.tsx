1cd5680e57127660c5a28a329f53be722c9af5f6
amirrezajm <amirreza.jolani@yahoo.com>
2025-04-02 11:48:55 +0200

    remove the script from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,12 +50,6 @@ export default function RootLayout({
           `,
           }}
         />
-        <Script id="vector-script" strategy="afterInteractive">
-          {`
-            !function(e,r){try{if(e.vector)return void console.log("Vector snippet included more than once.");var t={};t.q=t.q||[];for(var o=["load","identify","on"],n=function(e){return function(){var r=Array.prototype.slice.call(arguments);t.q.push([e,r])}},c=0;c<o.length;c++){var a=o[c];t[a]=n(a)}if(e.vector=t,!t.loaded){var i=r.createElement("script");i.type="text/javascript",i.async=!0,i.src="https://cdn.vector.co/pixel.js";var l=r.getElementsByTagName("script")[0];l.parentNode.insertBefore(i,l),t.loaded=!0}}catch(e){console.error("Error loading Vector:",e)}}(window,document);
-            vector.load("49b4809e-18ba-4ea6-8a23-bad02819a6b8");
-          `}
-        </Script>
       </head>
       <body className={`${montserrat.variable} antialiased`}>
         <ThemeProvider
