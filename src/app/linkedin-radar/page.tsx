0d6d9dfc4c5fe73fe285d14b2cd76b3a4b807481
amirrezajm <amirreza.jolani@yahoo.com>
2025-05-09 15:02:29 +0200

    i added the use client to linkedin page.

--- src/app/linkedin-radar/page.tsx
+++ src/app/linkedin-radar/page.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 import React, { useEffect, useState, useRef } from 'react';
 import { Radar, Search, Filter, Mail, Users, Briefcase, UserCheck, Shield } from 'lucide-react';
 import { Button } from "@/components/ui/button";
